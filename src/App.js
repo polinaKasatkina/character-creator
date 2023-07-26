49f08bf84f52a00627805253f20eb7672df3667a
Polina Kasatkina <p.s.kasatkina@gmail.com>
2023-07-26 15:50:39 +0400

    Workshop: Character creator

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import React from 'react';
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
 
+const StackingBg = {
+  backgroundColor: 'hsl(195deg, 20%, 86%)',
+  width: '100%',
+  height: '40%',
+  position: 'fixed',
+  bottom: 0
+};
+
 function App() {
   return (
     <>
+      <div style={StackingBg}></div>
       <CharacterEditor />
       <Footer />
     </>
